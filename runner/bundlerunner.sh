7dd81aaf4405e9b7ecc2d17e62b7f722ed240c5a
Robert Zondervan <robert@conduction.nl>
2021-06-21 13:08:29 +0200

    Fix updates

--- runner/bundlerunner.sh
+++ runner/bundlerunner.sh
@@ -37,35 +37,48 @@ run_docker() {
 }
 
 bundle_tasks() {
-	docker-compose exec -T php composer req conduction/commongroundbundle:v2-dev
+	docker-compose exec -T php composer req conduction/commongroundbundle:^2.*-dev
+	docker-compose exec -T php composer req symfony/asset:^5.1 symfony/console:5.1 symfony/dotenv:^5.1 symfony/framework-bundle:^5.1 symfony/property-access:^5.1 symfony/translation:^5.1 symfony/yaml:^5.1 phpdocumentor/reflection-docblock:^5.1
+	docker-compose exec -T php composer req --dev phpunit/phpunit:^9.3 phar-io/manifest:^2.0.1 phar-io/version:^3.0.1 phpunit/php-code-coverage:^9 symfony/phpunit-bridge:^5.1 phpspec/prophecy:^1.11.1
 	sleep 30
 
 	cp ./api/vendor/conduction/commongroundbundle/Resources/views/repo/common_ground.yaml ./api/config/packages/
 	echo "copied common_ground.yaml"
 
-	cp ./api/vendor/conduction/commongroundbundle/Resources/views/repo/.bettercodehub.yaml ./api
+	cp ./api/vendor/conduction/commongroundbundle/Resources/views/repo/.bettercodehub.yml ./api
 	echo "copied .bettercodehub.yaml"
 	
+	cp ./api/vendor/conduction/commongroundbundle/Resources/views/helm/api-deployment.yaml ./api/helm/templates
+	cp ./api/vendor/conduction/commongroundbundle/Resources/views/helm/api-service.yaml ./api/helm/templates
+	cp ./api/vendor/conduction/commongroundbundle/Resources/views/helm/certificate.yaml ./api/helm/templates
 	cp ./api/vendor/conduction/commongroundbundle/Resources/views/helm/configmap.yaml ./api/helm/templates
+	cp ./api/vendor/conduction/commongroundbundle/Resources/views/helm/dependencies-secret.yaml ./api/helm/templates
+	cp ./api/vendor/conduction/commongroundbundle/Resources/views/helm/ingress.yaml ./api/helm/templates
 	cp ./api/vendor/conduction/commongroundbundle/Resources/views/helm/_helpers.tpl ./api/helm/templates
 	cp ./api/vendor/conduction/commongroundbundle/Resources/views/helm/php-deployment.yaml ./api/helm/templates
+	cp ./api/vendor/conduction/commongroundbundle/Resources/views/helm/php-service.yaml ./api/helm/templates
 	cp ./api/vendor/conduction/commongroundbundle/Resources/views/helm/secrets.yaml ./api/helm/templates
 	echo "copied helm templates"
 
 	cp ./api/vendor/conduction/commongroundbundle/Resources/views/repo/docker-entrypoint.sh ./api/docker/php
+	cp ./api/vendor/conduction/commongroundbundle/Resources/views/repo/default.conf.template ./api/docker/nginx/conf.d
 	cp ./api/vendor/conduction/commongroundbundle/Resources/views/repo/Dockerfile ./api
 	cp ./api/vendor/conduction/commongroundbundle/Resources/views/repo/Dockerfile-nginx ./api
 	cp ./api/vendor/conduction/commongroundbundle/Resources/views/repo/docker-compose.yml .
 	echo "copied docker stuff"
 
+  rm ./api/config/bootstrap.php
+	cp ./api/vendor/conduction/commongroundbundle/Resources/views/repo/bootstrap.php ./api/config
+	echo "copied new bootstrap"
+
 	docker-compose exec -T php bin/console app:documentation:generate
 	sleep 5 
 
-	cp ./api/documentation/readme.md .
-	echo "copied readme.md"
-
-  cp ./api/documentation/helm/readme.md ./api/helm
-	echo "copied readme.md"
+#	cp ./api/documentation/readme.md .
+#	echo "copied readme.md"
+#
+#  cp ./api/documentation/helm/readme.md ./api/helm
+#	echo "copied readme.md"
 
 	cp ./api/documentation/artifacthub-repo.yaml ./api/helm
 	cp ./api/documentation/values.schema.json ./api/helm
@@ -87,6 +100,8 @@ bundle_tasks() {
 	docker-compose down
 
 	rm api/helm/*.tgz
+	docker run -ti alpine/helm:3.2.1 dependency update -v $(pwd)/api/helm:/app
+	sed -i 's#\/\*.tgz#.\/\*.tgz#g' ./api/helm/.helmignore
 
 	docker-compose up -d
 
@@ -111,6 +126,7 @@ run_update() {
 	local branch=$(get_repo $1 $2)
 	run_docker $2
 	bundle_tasks
+	cd ..
 	run_docker $2
 	git_update $1 $branch $2
 	cd ..
