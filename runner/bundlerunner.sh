33e9ba079d09f70b9ad5cd979ffffd99b38a66d3
Robert Zondervan <robert@conduction.nl>
2021-06-21 16:47:22 +0200

    Definitive version

--- runner/bundlerunner.sh
+++ runner/bundlerunner.sh
@@ -39,7 +39,7 @@ run_docker() {
 bundle_tasks() {
 	docker-compose exec -T php composer req conduction/commongroundbundle:^2.*-dev
 	docker-compose exec -T php composer req symfony/asset:^5.1 symfony/console:5.1 symfony/dotenv:^5.1 symfony/framework-bundle:^5.1 symfony/property-access:^5.1 symfony/translation:^5.1 symfony/yaml:^5.1 phpdocumentor/reflection-docblock:^5.1
-	docker-compose exec -T php composer req --dev phpunit/phpunit:^9.3 phar-io/manifest:^2.0.1 phar-io/version:^3.0.1 phpunit/php-code-coverage:^9 symfony/phpunit-bridge:^5.1 phpspec/prophecy:^1.11.1
+	docker-compose exec -T php composer req --dev phpunit/phpunit:^9.3 phar-io/manifest:^2.0.1 phar-io/version:^3.0.1 phpunit/php-code-coverage:^9 symfony/phpunit-bridge:^5.1 phpspec/prophecy:^1.11.1 sebastian/global-state:^5.0
 	sleep 30
 
 	cp ./api/vendor/conduction/commongroundbundle/Resources/views/repo/common_ground.yaml ./api/config/packages/
@@ -70,6 +70,8 @@ bundle_tasks() {
   rm ./api/config/bootstrap.php
 	cp ./api/vendor/conduction/commongroundbundle/Resources/views/repo/bootstrap.php ./api/config
 	echo "copied new bootstrap"
+
+	cp ./api/vendor/conduction/commongroundbundle/Resources/views/workflows/dockerimage.yml ./.github/workflows
 
 	docker-compose exec -T php bin/console app:documentation:generate
 	sleep 5 
@@ -103,7 +105,7 @@ bundle_tasks() {
 	rm api/helm/templates/varnish-service.yaml
 	rm api/helm/templates/api-loadbalancer.yaml
 	rm api/helm/*.tgz
-	docker run -ti alpine/helm:3.2.1 dependency update -v $(pwd)/api/helm:/app
+	docker run -t -v $(pwd)/api/helm:/app alpine/helm:3.2.1 dependency update /app
 	sed -i 's#^/\*.tgz#./\*.tgz#g' ./api/helm/.helmignore
 
 	docker-compose up -d
@@ -131,6 +133,7 @@ run_update() {
 	bundle_tasks
 	cd ..
 	run_docker $2
+	docker-compose down
 	git_update $1 $branch $2
 	cd ..
 }
